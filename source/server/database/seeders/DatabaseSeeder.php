e9512c5e2098eda8b561e4dbfbf9a8b087864e62
NGUYEN TRONG DAI <nguyendaj36@gmail.com>
2026-04-14 21:58:27 +0700

    commit 3

--- source/server/database/seeders/DatabaseSeeder.php
+++ source/server/database/seeders/DatabaseSeeder.php
@@ -5,21 +5,23 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash; // Khai báo thư viện Hash ở đây
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+    // Bỏ comment dòng dưới nếu bạn muốn tắt các event model khi chạy seed
+    // use WithoutModelEvents; 
 
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Admin Super',
+            'email' => 'test@gmail.com', // Bạn có thể đổi lại thành admin@gmail.com nếu cần
+            'password' => Hash::make('123456'),
+            'role' => 'admin'
         ]);
     }
 }
